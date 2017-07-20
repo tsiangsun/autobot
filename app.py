1cf9508cc48c7e9054ad32c0c439064c48ce4e02
Xiang Sun <tsiangsun@gmail.com>
2017-07-19 22:24:18 -0700

    updated numpy scipy

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def index():
 #========================================================================================   
 @app.route('/value', methods=['POST'])
 def carvalue():
-    import numpy 
+    import numpy as np
     from sklearn import base
     from sklearn import datasets, linear_model, utils, preprocessing
     from sklearn.base import BaseEstimator, RegressorMixin, TransformerMixin
